7648c25d38b104644febf1606700faca9bc31e88
Dimas Raka Septiawan ⚡️ <dimasrakas@gmail.com>
2021-04-25 18:41:49 +0700

    update content

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <h1 class="mt-6 text-lg font-semibold text-center">
       Pilih Official Store kami di bawah ini :
     </h1>
-    <div class="grid w-full max-w-lg grid-cols-2 gap-4 mt-4">
+    <div class="grid w-full max-w-lg grid-cols-3 gap-8 mt-4">
       <a
         href="https://shopee.co.id/orbitos"
         target="_blank"
@@ -14,45 +14,36 @@
         <img class="w-auto h-24" src="/social/shopee.png" alt="" />
       </a>
       <a
+        href="https://www.blibli.com/merchant/ShareStore/DI9-70000"
+        target="_blank"
+        class="flex items-center justify-center w-full p-4 bg-white rounded-lg cursor-pointer"
+      >
+        <img class="w-auto" src="/social/blibli.png" alt="" />
+      </a>
+      <a
         href="https://www.lazada.co.id/shop/orbitos"
         target="_blank"
         class="flex items-center justify-center w-full p-4 bg-white rounded-lg cursor-pointer"
       >
-        <img class="w-auto h-24" src="/social/tokopedia.png" alt="" />
-      </a>
-      <div
-        class="flex items-center justify-center w-full p-4 bg-white rounded-lg cursor-pointer"
-      >
-        <img class="w-auto h-24" src="/social/bukalapak.png" alt="" />
-      </div>
-      <div
-        class="flex items-center justify-center w-full p-4 bg-white rounded-lg cursor-pointer"
-      >
-        <img class="w-auto" src="/social/blibli.png" alt="" />
-      </div>
-      <div
-        class="flex items-center justify-center w-full p-4 bg-white rounded-lg cursor-pointer"
-      >
         <img class="w-auto h-24" src="/social/lazada.png" alt="" />
-      </div>
-      <div
-        class="flex items-center justify-center w-full p-4 bg-white rounded-lg cursor-pointer"
-      >
-        <img class="w-auto h-24" src="/social/jdid.png" alt="" />
-      </div>
+      </a>
 
-      <div
-        class="flex items-center w-full col-span-2 p-4 bg-white rounded-lg cursor-pointer"
+      <a
+        href="https://api.whatsapp.com/send?phone=6282139730230&text=Halo%20Orbitos%20"
+        target="_blank"
+        class="flex items-center w-full col-span-3 p-4 bg-white rounded-lg cursor-pointer"
       >
         <img class="w-auto h-8 mr-2" src="/social/wa.png" alt="" />
         <h1 class="text-xl font-semibold text-gray-800">Customer Services 1</h1>
-      </div>
-      <div
-        class="flex items-center w-full col-span-2 p-4 bg-white rounded-lg cursor-pointer"
+      </a>
+      <a
+        href="https://api.whatsapp.com/send?phone=6282333889997&text=Halo%20Orbitos%20"
+        target="_blank"
+        class="flex items-center w-full col-span-3 p-4 bg-white rounded-lg cursor-pointer"
       >
         <img class="w-auto h-8 mr-2" src="/social/wa.png" alt="" />
         <h1 class="text-xl font-semibold text-gray-800">Customer Services 2</h1>
-      </div>
+      </a>
     </div>
   </div>
 </template>
